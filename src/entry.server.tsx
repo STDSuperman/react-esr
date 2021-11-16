1862638812c77945921668eeab4331faf6fd760d
STDSuperman <2750556766@qq.com>
2021-11-16 23:21:55 +0800

    feat: 还原移除骨架屏能力

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -118,7 +118,7 @@ const getRemoveSkeletonScript = () => {
   return `
     <script>
       window.addEventListener('load', () => {
-        window.SKELETON && SKELETON.destroy;
+        window.SKELETON && SKELETON.destroy();
       })
     </script>
   `
